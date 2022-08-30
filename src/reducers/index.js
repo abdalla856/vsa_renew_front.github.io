4d3b85264791e8cbd42c3f212ca3f30f985387e3
abdalla mahmmoud <bodymoud1999@gmail.com>
2022-08-30 17:24:56 +0200

    last commit

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux';
-import apps from './app'
-import user from './auth'
-
-export default combineReducers({
-    apps,user
-})
--- src/view/shared/context/auth-context.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-
-export const  authCotext = createContext({isLoggedIn :false,
-      login :()=>{},
-      type:null
-      ,logout:()=>{},userId :null  , app :[],users:[]})
