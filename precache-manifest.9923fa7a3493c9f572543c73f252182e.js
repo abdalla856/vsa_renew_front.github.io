0551aee4d81974f3dcf612f5b580698d061a6878
abdalla mahmmoud <bodymoud1999@gmail.com>
2022-09-03 23:14:12 +0200

    Updates

---
+++ precache-manifest.9923fa7a3493c9f572543c73f252182e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b10f027793379a981ed62a6112bfe11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18a342c7d15933f5f8ee",
+    "url": "/static/css/2.3df0fce2.chunk.css"
+  },
+  {
+    "revision": "b5ae4159f982b8da2b80",
+    "url": "/static/css/main.330f80e3.chunk.css"
+  },
+  {
+    "revision": "18a342c7d15933f5f8ee",
+    "url": "/static/js/2.65e44758.chunk.js"
+  },
+  {
+    "revision": "b5ae4159f982b8da2b80",
+    "url": "/static/js/main.2dfa44ba.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  },
+  {
+    "revision": "e08f8b00704d429c90e151bad272dc28",
+    "url": "/static/media/cat.e08f8b00.png"
+  },
+  {
+    "revision": "f96d7eaf4c6d84dcbbf4e16a348d1b85",
+    "url": "/static/media/error.f96d7eaf.gif"
+  },
+  {
+    "revision": "aedf99af6cb6acae60b1c68f78806db8",
+    "url": "/static/media/progress.aedf99af.gif"
+  }
+]);
